8d7dc433ba230bd1a37d0ebecd0ffc8ebbd39ed7
Oleg Putseiko <oleg.putseiko@gmail.com>
2026-06-21 16:44:22 +0900

    feat(theme): add no color mode

--- scripts/bin/cli.sh
+++ scripts/bin/cli.sh
@@ -10,6 +10,19 @@
 
 BIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+export NO_COLOR=0
+NEW_ARGS=()
+
+for arg in "$@"; do
+  if [[ "$arg" == "--no-color" || "$arg" == "-nc" ]]; then
+    export NO_COLOR=1
+  else
+    NEW_ARGS+=("$arg")
+  fi
+done
+
+set -- "${NEW_ARGS[@]}"
+
 source "$BIN_DIR/core/root.sh"
 source "$BIN_DIR/core/theme.sh"
 
